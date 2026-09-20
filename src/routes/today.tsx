e8bd1d79681eb329fa83cc0504d942e7dab9f119
mounsokdara <mounsokdara@gmail.com>
2026-09-20 07:39:51 +0700

    Delete src directory

--- src/routes/today.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/today")({
-  beforeLoad: () => {
-    throw redirect({ to: "/day" });
-  },
-});
